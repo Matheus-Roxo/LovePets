429f7483942760c25a0da5791ac11c549b798fa7
Matheus Roxo Brandão <44602584802@info.local>
2021-07-19 10:57:59 -0300

    Repositories quase finalizado

---
+++ sprint-2-back-end/lovePets_webApi/senai_lovePets_webApi/Repositories/DonoRepository.cs
@@ -0,0 +1,49 @@
+﻿using senai_lovePets_webApi.Contexts;
+using senai_lovePets_webApi.Domains;
+using senai_lovePets_webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_lovePets_webApi.Repositories
+{
+    public class DonoRepository : IDonoRepository
+    {
+        lovePetsContext ctx = new lovePetsContext();
+
+        public void Atualizar(int idDono, Dono donoAtualizado)
+        {
+            Dono donoBuscar = BuscarPorId(idDono);
+
+            if (donoAtualizado.NomeDono != null)
+            {
+                donoBuscar.NomeDono = donoAtualizado.NomeDono;
+            }
+        }
+
+        public Dono BuscarPorId(int idDono)
+        {
+            return ctx.Dono.Find(idDono);
+        }
+
+        public void Cadastrar(Dono novoDono)
+        {
+            ctx.Dono.Add(novoDono);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int idDono)
+        {
+            ctx.Dono.Remove(BuscarPorId(idDono));
+
+            ctx.SaveChanges();
+        }
+
+        public List<Dono> ListarTodos()
+        {
+            return ctx.Dono.ToList();
+        }
+    }
+};
---
+++ sprint-2-back-end/lovePets_webApi/senai_lovePets_webApi/Repositories/RacaRepository.cs
@@ -0,0 +1,42 @@
+﻿using senai_lovePets_webApi.Domains;
+using senai_lovePets_webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_lovePets_webApi.Repositories
+{
+    public class RacaRepository : IRacaRepository
+    {
+        public void Atualizar(int idRaca, Raca RacaAtualizada)
+        {
+            Raca racaBuscar = BuscarPorId(idRaca);
+
+            if (RacaAtualizada.NomeRaca != null)
+            {
+               racaBuscar.NomeRaca = RacaAtualizada.NomeRaca;
+            }
+        }
+
+        public Raca BuscarPorId(int idRaca)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Raca novaRaca)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idRaca)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Raca> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
